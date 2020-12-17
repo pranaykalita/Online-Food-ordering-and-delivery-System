5f6438f8cb85a34317f70e9c1088465ba3cc6b33
n0v4c0r3 <pranaykalita2@gmail.com>
2020-12-18 03:26:57 +0530

    update

---
+++ cart.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<?php include 'php/connection.php' ?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CART</title>
+    <link rel="stylesheet" href="css/cart.css">
+</head>
+<body class="u-body u-overlap u-overlap-transparent">
+<?php include 'header.php' ?>  
+<section class="cart">
+
+
+</section>  
+<?php include 'footer.php' ?>    
+</body>
+</html>
--- login.php
+++ login.php
@@ -62,7 +62,7 @@
         
         <div
             class="container-login100"
-            style="background-image: url('/images/bg-01.jpg');">
+            style="background-image: url('/images/loginimage.jpg');">
             <div class="wrap-login100 p-l-55 p-r-55 p-t-80 p-b-30">
                 <form action="" method="post" class="login100-form validate-form"> 
                     <span class="login100-form-title p-b-37">
