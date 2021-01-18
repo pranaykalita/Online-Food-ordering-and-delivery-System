2879f971db3dace835578cfc2bb96ccba16cd44d
n0v4c0r3 <pranaykalita2@gmail.com>
2021-01-18 21:10:50 +0530

    update

--- admin/index.php
+++ admin/index.php
@@ -108,7 +108,7 @@ $staff = $result[0];
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
                             <div class="text-xs font-weight-bold text-info text-uppercase mb-1">
-                                Staff available</div>
+                                Total Employees</div>
                             <div class="h5 mb-0 font-weight-bold text-gray-800">5</div>
                         </div>
                         <div class="col-auto">
